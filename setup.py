4a1675c608b1dd371fcb9d28925135ebfc3b71f6
Suryasadasivam <suryasadasivamm@gmail.com>
2025-02-27 12:01:43 +0530

    second commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+# Building a application like package so we are using setup 
+
+from setuptools import find_packages,setup 
+from typing import List
+
+HYPEN_E_DOT="-e ."
+
+
+def get_requirements(file_path:str)->List[str]:
+    #this function will return a list of requirements 
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+
+setup(
+    name ='mlproject',
+    version ='0.0.1',
+    author="Surya",
+    author_email="surya@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    )
